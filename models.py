f96e1eb2b3c3c3243327228369f58c6bc7dbae5e
LucasI27 <luina.12042003@gmail.com>
2019-11-07 10:19:36 -0300

    comecei a  estruturar o banco de dados

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+class usuarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    msg = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<MSG %r>' % self.msg
+
+
