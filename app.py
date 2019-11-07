f495596ada099665ca6b2d34bb6dc9daf9ea952f
LucasI27 <luina.12042003@gmail.com>
2019-11-07 08:37:21 -0300

    adicionei renderTemplate

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+app.run(debug=True)
